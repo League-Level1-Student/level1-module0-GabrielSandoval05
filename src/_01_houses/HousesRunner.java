6784061fa74b565cb454fba5fe42f1e14cc841c4
351054 <351054@0338S-Z-90TDE0K.sdcs.local>
2019-12-03 14:22:24 -0800

    12/3/19

--- src/_01_houses/HousesRunner.java
+++ src/_01_houses/HousesRunner.java
@@ -18,17 +18,18 @@ meme.setSpeed(10);
 meme.penDown();
 String house = JOptionPane.showInputDialog("What height would you like your houses? Small Medium or Large");
  //int hi = Integer.parseInt(house);
+String color2 = JOptionPane.showInputDialog("What color would you like them? Teal, Green, Magenta, or Mystery?");
 
 
 
-drawHouse(house);
+drawHouse(house,color2);
 
 
 	}
-	public static void drawHouse(String height2) {
+	public static void drawHouse(String height2, String color) {
 		int height = 0;
 		if(height2.equalsIgnoreCase("small")) {
-			height=50;
+			height=60;
 		}
 		else if(height2.equalsIgnoreCase("medium")) {
 			height=120;
@@ -38,6 +39,21 @@ drawHouse(house);
 		}
 		else {
 			JOptionPane.showMessageDialog(null, "K thx");
+		}
+		if(color.equalsIgnoreCase("green")) {
+		meme.setPenColor(66, 255, 90);
+		}
+		else if(color.equalsIgnoreCase("Teal")) {
+			meme.setPenColor(17, 240, 177);
+		}
+		else if(color.equalsIgnoreCase("Magenta")) {
+			meme.setPenColor(245, 7, 205);
+		}
+		else if(color.equalsIgnoreCase("Mystery")) {
+			meme.setRandomPenColor();
+		}
+		else {
+			System.out.println("ok funny joke but there aren't any more colors");
 		}
 	for(int i = 0; i<9;i++) {
 		meme.move(height);
@@ -49,7 +65,22 @@ drawHouse(house);
 		meme.setPenColor(59, 130, 49);
 		meme.move(30);
 		meme.turn(-90);
-		meme.setPenColor(0, 0, 0);
+		meme.setPenColor(0,0,0);
+		if(color.equalsIgnoreCase("green")) {
+			meme.setPenColor(66, 255, 90);
+			}
+			else if(color.equalsIgnoreCase("Teal")) {
+				meme.setPenColor(17, 240, 177);
+			}
+			else if(color.equalsIgnoreCase("Magenta")) {
+				meme.setPenColor(245, 7, 205);
+			}
+			else if(color.equalsIgnoreCase("Mystery")) {
+				meme.setRandomPenColor();
+			}
+			else {
+				System.out.println("ok funny joke but there aren't any more colors");
+			}
 	}
 
 	}
